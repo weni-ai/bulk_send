2e1e1048a2cdc226e803013cc788b38b9d6eb7a0
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-08-05 16:25:19 -0300

    Add base components structure

---
+++ src/components/HomeBulkSend/HomeHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="header">
+    <div class="header-content">
+      <h1 class="title">{{ $t('home.title') }}</h1>
+      <p class="description">{{ $t('home.description') }}</p>
+    </div>
+
+    <UnnnicButton :text="$t('home.new_send')" type="primary" iconLeft="add-1" />
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: $unnnic-spacing-sm;
+
+  & .header-content {
+    display: flex;
+    flex-direction: column;
+    gap: $unnnic-spacing-sm;
+    width: 100%;
+  }
+
+  & .title {
+    color: $unnnic-color-neutral-darkest;
+    font-size: $unnnic-font-size-title-sm;
+    font-weight: $unnnic-font-weight-bold;
+    line-height: $unnnic-font-size-title-sm + $unnnic-line-height-md;
+  }
+
+  & .description {
+    color: $unnnic-color-neutral-darkest;
+    font-size: $unnnic-font-size-body-gt;
+    font-weight: $unnnic-font-weight-regular;
+    line-height: $unnnic-font-size-body-gt + $unnnic-line-height-md;
+  }
+}
+</style>
---
+++ src/layouts/BulkSend/BulkSendHomeLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <section :class="`bulk-send-home-layout`">
+    <slot class="header" name="header" />
+    <div class="divider"></div>
+    <slot class="content" name="content" />
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.bulk-send-home-layout {
+  height: 100%;
+  padding: $unnnic-spacing-sm;
+
+  & .divider {
+    height: $unnnic-border-width-thinner;
+    background-color: $unnnic-color-neutral-soft;
+    margin: $unnnic-spacing-md 0;
+  }
+}
+</style>
