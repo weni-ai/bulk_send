58440d463450d6835e8404e47854bc46508b5e22
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-08-06 12:00:39 -0300

    Refactor HomeHeader and BulkSendHomeLayout components to improve BEM compliance by updating class names and structure for better readability and maintainability.

--- src/layouts/BulkSend/BulkSendHomeLayout.vue
+++ src/layouts/BulkSend/BulkSendHomeLayout.vue
@@ -1,8 +1,8 @@
 <template>
   <section :class="`bulk-send-home-layout`">
-    <slot class="header" name="header" />
-    <div class="divider"></div>
-    <slot class="content" name="content" />
+    <slot class="bulk-send-home-layout__header" name="header" />
+    <div class="bulk-send-home-layout__divider"></div>
+    <slot class="bulk-send-home-layout__content" name="content" />
   </section>
 </template>
 
@@ -13,7 +13,7 @@
   height: 100%;
   padding: $unnnic-spacing-sm;
 
-  & .divider {
+  &__divider {
     height: $unnnic-border-width-thinner;
     background-color: $unnnic-color-neutral-soft;
     margin: $unnnic-spacing-md 0;
