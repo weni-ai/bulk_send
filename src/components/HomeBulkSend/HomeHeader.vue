f6595fd946a14b805ac0907009d94f586c301630
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-08-07 15:18:13 -0300

    Refactor class names in HomeHeader component to enhance BEM compliance and improve code readability.

--- src/components/HomeBulkSend/HomeHeader.vue
+++ src/components/HomeBulkSend/HomeHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="header">
     <div class="header__content">
-      <h1 class="header__content__title">{{ $t('home.title') }}</h1>
-      <p class="header__content__description">{{ $t('home.description') }}</p>
+      <h1 class="header__title">{{ $t('home.title') }}</h1>
+      <p class="header__description">{{ $t('home.description') }}</p>
     </div>
 
     <UnnnicButton :text="$t('home.new_send')" type="primary" iconLeft="add-1" />
@@ -23,20 +23,20 @@
     flex-direction: column;
     gap: $unnnic-spacing-sm;
     width: 100%;
+  }
 
-    &__title {
-      color: $unnnic-color-neutral-darkest;
-      font-size: $unnnic-font-size-title-sm;
-      font-weight: $unnnic-font-weight-bold;
-      line-height: $unnnic-font-size-title-sm + $unnnic-line-height-md;
-    }
+  &__title {
+    color: $unnnic-color-neutral-darkest;
+    font-size: $unnnic-font-size-title-sm;
+    font-weight: $unnnic-font-weight-bold;
+    line-height: $unnnic-font-size-title-sm + $unnnic-line-height-md;
+  }
 
-    &__description {
-      color: $unnnic-color-neutral-darkest;
-      font-size: $unnnic-font-size-body-gt;
-      font-weight: $unnnic-font-weight-regular;
-      line-height: $unnnic-font-size-body-gt + $unnnic-line-height-md;
-    }
+  &__description {
+    color: $unnnic-color-neutral-darkest;
+    font-size: $unnnic-font-size-body-gt;
+    font-weight: $unnnic-font-weight-regular;
+    line-height: $unnnic-font-size-body-gt + $unnnic-line-height-md;
   }
 }
 </style>
