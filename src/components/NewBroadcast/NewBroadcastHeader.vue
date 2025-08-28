b81126b41ccd8da50c848a7b80fe42241abb46a8
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-08-28 15:32:10 -0300

    feat: add NewBroadcastHeader component for new broadcast page layout and navigation

---
+++ src/components/NewBroadcast/NewBroadcastHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="new-broadcast-header">
+    <section class="new-broadcast-header__title-container">
+      <UnnnicButton
+        class="new-broadcast-header__back-button"
+        type="tertiary"
+        iconLeft="arrow_back"
+        @click="() => router.back()"
+      />
+      <h1 class="new-broadcast-header__title-text">
+        {{ $t('new_broadcast.title') }}
+      </h1>
+    </section>
+    <section class="new-broadcast-header__navigator">
+      <NewBroadcastNavigator />
+    </section>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import NewBroadcastNavigator from '@/components/NewBroadcast/NewBroadcastNavigator.vue';
+
+const router = useRouter();
+</script>
+
+<style scoped lang="scss">
+.new-broadcast-header {
+  display: flex;
+  flex-direction: column;
+  gap: $unnnic-spacing-sm;
+
+  &__title-container {
+    display: flex;
+    align-items: center;
+    gap: $unnnic-spacing-sm;
+  }
+
+  &__title-text {
+    color: $unnnic-color-neutral-darkest;
+    font-weight: $unnnic-font-weight-bold;
+    font-size: $unnnic-font-size-title-sm;
+    line-height: $unnnic-font-size-title-sm + $unnnic-line-height-md;
+  }
+}
+</style>
