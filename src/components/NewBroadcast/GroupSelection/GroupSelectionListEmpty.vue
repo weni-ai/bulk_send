a38fb004e2982b85b4480a229d4401c8fc5cfdf7
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-08-28 15:14:32 -0300

    feat: add GroupSelectionListEmpty component and corresponding unit tests for empty state rendering

---
+++ src/components/NewBroadcast/GroupSelection/GroupSelectionListEmpty.vue
@@ -0,0 +1,15 @@
+<template>
+  <section
+    class="group-selection-list__empty"
+    data-test="empty"
+  >
+    <p
+      class="group-selection-list__empty-text"
+      data-test="empty-text"
+    >
+      {{ $t('new_broadcast.pages.select_groups.no_groups') }}
+    </p>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
