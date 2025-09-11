438b5b7644a8a42d618a7b9a96a57d759d87d298
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-09-11 17:12:22 -0300

    refactor: replace GroupSelection and ContactImport components with GroupsStep for improved structure in NewBroadcast workflow

--- src/views/BulkSend/NewBroadcast.vue
+++ src/views/BulkSend/NewBroadcast.vue
@@ -9,23 +9,10 @@
           v-if="isSelectGroupsPage"
           class="new-broadcast-layout__contacts"
         >
-          <section
-            v-if="!hasContactImport || !uploadFinished"
-            class="new-broadcast-layout__content-groups"
-          >
-            <GroupSelection
-              class="new-broadcast-layout__group-selection"
-              :open="groupSelectionOpen"
-              @update:open="handleGroupSelectionOpen"
-            />
-            <ContactImport
-              :open="contactImportOpen"
-              @update:open="handleContactImportOpen"
-              @uploaded="handleUploaded"
-            />
-          </section>
-
-          <ContactImportProcessing v-if="hasContactImport && uploadFinished" />
+          <GroupsStep
+            :uploadFinished="uploadFinished"
+            @update:upload-finished="(v: boolean) => (uploadFinished = v)"
+          />
         </section>
         <section
           v-if="isSelectTemplatePage"
@@ -33,23 +20,18 @@
         >
           <TemplateSelection data-test="template-selection" />
         </section>
-      </section>
-      <!-- TODO: Move this into each step page for easier management? Answer: YES PLEASE -->
-      <section class="new-broadcast-layout__content-actions">
-        <UnnnicButton
-          class="new-broadcast-layout__content-actions-cancel"
-          type="tertiary"
-          @click="handleCancel"
+        <section
+          v-if="isSelectVariablesPage"
+          class="new-broadcast-layout__variables"
         >
-          {{ $t('new_broadcast.pages.actions.cancel') }}
-        </UnnnicButton>
-        <UnnnicButton
-          class="new-broadcast-layout__content-actions-continue"
-          :disabled="!canContinue"
-          @click="handleContinue"
+          <VariablesSelection data-test="variables-selection" />
+        </section>
+        <section
+          v-if="isConfirmAndSendPage"
+          class="new-broadcast-layout__confirm-and-send"
         >
-          {{ $t('new_broadcast.pages.actions.continue') }}
-        </UnnnicButton>
+          <ConfirmAndSend data-test="confirm-and-send" />
+        </section>
       </section>
     </template>
   </NewBroadcastLayout>
@@ -57,37 +39,30 @@
 
 <script setup lang="ts">
 import { computed, onBeforeMount, ref } from 'vue';
-import { useRouter } from 'vue-router';
 import { useBroadcastsStore } from '@/stores/broadcasts';
 import { useContactImportStore } from '@/stores/contactImport';
 import { useProjectStore } from '@/stores/project';
+import { useTemplatesStore } from '@/stores/templates';
 import { NewBroadcastPage } from '@/constants/broadcasts';
 import NewBroadcastLayout from '@/layouts/BulkSend/NewBroadcastLayout.vue';
 import NewBroadcastHeader from '@/components/NewBroadcast/NewBroadcastHeader.vue';
-import GroupSelection from '@/components/NewBroadcast/GroupSelection/GroupSelection.vue';
-import ContactImport from '@/components/NewBroadcast/ContactImport/ContactImport.vue';
-import ContactImportProcessing from '@/components/NewBroadcast/ContactImport/ContactImportProcessing.vue';
+import GroupsStep from '@/components/NewBroadcast/GroupSelection/GroupsStep.vue';
 import TemplateSelection from '@/components/NewBroadcast/TemplateSelection/TemplateSelection.vue';
-import { TemplateStatus } from '@/constants/templates';
-
-const router = useRouter();
+import VariablesSelection from '@/components/NewBroadcast/VariablesSelection/VariablesSelection.vue';
+import ConfirmAndSend from '@/components/NewBroadcast/ConfirmAndSend/ConfirmAndSend.vue';
 
 const broadcastsStore = useBroadcastsStore();
 const contactImportStore = useContactImportStore();
 const projectStore = useProjectStore();
+const templatesStore = useTemplatesStore();
 
 const uploadFinished = ref(false);
 
 onBeforeMount(() => {
   broadcastsStore.setNewBroadcastPage(NewBroadcastPage.SELECT_GROUPS);
+  projectStore.getProjectInfo();
+  templatesStore.getTemplatePricing();
 });
-
-const groupSelectionOpen = computed(
-  () => broadcastsStore.newBroadcast.groupSelectionOpen,
-);
-const contactImportOpen = computed(
-  () => broadcastsStore.newBroadcast.contactImportOpen,
-);
 
 const isSelectGroupsPage = computed(
   () =>
@@ -99,83 +74,21 @@ const isSelectTemplatePage = computed(
     broadcastsStore.newBroadcast.currentPage ===
     NewBroadcastPage.SELECT_TEMPLATE,
 );
-const hasContactImport = computed(() => contactImportStore.import);
 
-const canContinue = computed(() => {
-  if (isSelectGroupsPage.value) {
-    return (
-      broadcastsStore.newBroadcast.selectedGroups.length > 0 ||
-      (contactImportStore.import && uploadFinished.value)
-    );
-  }
-
-  if (isSelectTemplatePage.value) {
-    return (
-      broadcastsStore.newBroadcast.selectedTemplate &&
-      broadcastsStore.newBroadcast.selectedTemplate.status ===
-        TemplateStatus.APPROVED
-    );
-  }
-
-  return false;
-});
-
-const canConfirmImport = computed(() => {
-  return (
+const isSelectVariablesPage = computed(
+  () =>
     broadcastsStore.newBroadcast.currentPage ===
-      NewBroadcastPage.SELECT_GROUPS && contactImportStore.import
-  );
-});
+    NewBroadcastPage.SELECT_VARIABLES,
+);
 
-const handleGroupSelectionOpen = (value: boolean) => {
-  broadcastsStore.setGroupSelectionOpen(value);
-  broadcastsStore.setContactImportOpen(!value);
-};
-
-const handleContactImportOpen = (value: boolean) => {
-  broadcastsStore.setContactImportOpen(value);
-  broadcastsStore.setGroupSelectionOpen(!value);
-};
-
-const handleUploaded = () => {
-  uploadFinished.value = true;
-};
-
-const handleCancel = () => {
-  if (!canContinue.value) {
-    // return to previous page
-    router.back();
-  }
-
-  if (contactImportStore.import) {
-    contactImportStore.clearImport();
-  }
-
-  broadcastsStore.setNewBroadcastPage(NewBroadcastPage.SELECT_GROUPS);
-};
-
-// TODO: Refactor this to avoid so many if-elses
-const handleContinue = () => {
-  if (isSelectGroupsPage.value && canConfirmImport.value) {
-    contactImportStore.confirmContactImport(
-      projectStore.project.uuid,
-      contactImportStore.import!.importId,
-      contactImportStore.importProcessing,
-    );
-
-    broadcastsStore.setNewBroadcastPage(NewBroadcastPage.SELECT_TEMPLATE);
-  } else if (
-    isSelectGroupsPage.value &&
-    broadcastsStore.newBroadcast.selectedGroups.length > 0
-  ) {
-    broadcastsStore.setNewBroadcastPage(NewBroadcastPage.SELECT_TEMPLATE);
-  } else if (isSelectTemplatePage.value) {
-    broadcastsStore.setNewBroadcastPage(NewBroadcastPage.SELECT_VARIABLES);
-  }
-};
+const isConfirmAndSendPage = computed(
+  () =>
+    broadcastsStore.newBroadcast.currentPage ===
+    NewBroadcastPage.CONFIRM_AND_SEND,
+);
 
 const handleBack = () => {
-  broadcastsStore.setNewBroadcastPage(NewBroadcastPage.SELECT_GROUPS);
+  broadcastsStore.resetNewBroadcast();
   contactImportStore.clearImport();
   uploadFinished.value = false;
 };
@@ -183,23 +96,19 @@ const handleBack = () => {
 
 <style scoped lang="scss">
 .new-broadcast-layout {
-  &__group-selection::after {
-    content: '';
-    display: block;
-    width: 100%;
-    height: $unnnic-border-width-thinner;
-    background-color: $unnnic-color-neutral-soft;
-    margin: $unnnic-spacing-md 0;
-  }
-
-  &__content-actions {
+  &__content {
     display: flex;
-    margin-top: auto;
+    flex-direction: column;
     gap: $unnnic-spacing-sm;
+    flex: 1;
   }
 
-  &__content-actions-cancel,
-  &__content-actions-continue {
+  &__contacts,
+  &__template,
+  &__variables,
+  &__confirm-and-send {
+    display: flex;
+    flex-direction: column;
     flex: 1;
   }
 }
