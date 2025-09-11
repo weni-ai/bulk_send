2684d218e473a65f62664c8202041593c4224209
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-09-11 16:48:03 -0300

    feat: add StepActions and GroupsStep components for managing group selection and contact import in the New Broadcast workflow

---
+++ src/components/NewBroadcast/GroupSelection/GroupsStep.vue
@@ -0,0 +1,138 @@
+<template>
+  <section class="groups-step">
+    <section
+      v-if="!hasContactImport || !uploadFinished"
+      class="groups-step__selection"
+    >
+      <GroupSelection
+        class="groups-step__group-selection"
+        :open="groupSelectionOpen"
+        @update:open="handleGroupSelectionOpen"
+      />
+      <ContactImport
+        :open="contactImportOpen"
+        @update:open="handleContactImportOpen"
+        @uploaded="handleUploaded"
+      />
+    </section>
+
+    <ContactImportProcessing v-if="hasContactImport && uploadFinishedProp" />
+
+    <StepActions
+      :disabled="!canContinue"
+      @cancel="handleCancel"
+      @continue="handleContinue"
+    />
+  </section>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useBroadcastsStore } from '@/stores/broadcasts';
+import { useContactImportStore } from '@/stores/contactImport';
+import { useProjectStore } from '@/stores/project';
+import { NewBroadcastPage } from '@/constants/broadcasts';
+import GroupSelection from '@/components/NewBroadcast/GroupSelection/GroupSelection.vue';
+import ContactImport from '@/components/NewBroadcast/ContactImport/ContactImport.vue';
+import ContactImportProcessing from '@/components/NewBroadcast/ContactImport/ContactImportProcessing.vue';
+import StepActions from '@/components/NewBroadcast/StepActions.vue';
+
+const broadcastsStore = useBroadcastsStore();
+const contactImportStore = useContactImportStore();
+const projectStore = useProjectStore();
+
+const props = defineProps<{
+  uploadFinished: boolean;
+}>();
+const emit = defineEmits<{
+  (e: 'update:upload-finished', value: boolean): void;
+}>();
+
+const uploadFinishedProp = computed(() => props.uploadFinished);
+
+const groupSelectionOpen = computed(
+  () => broadcastsStore.newBroadcast.groupSelectionOpen,
+);
+const contactImportOpen = computed(
+  () => broadcastsStore.newBroadcast.contactImportOpen,
+);
+
+const hasContactImport = computed(() => contactImportStore.import);
+
+const canConfirmImport = computed(() => {
+  return (
+    broadcastsStore.newBroadcast.currentPage ===
+      NewBroadcastPage.SELECT_GROUPS && contactImportStore.import
+  );
+});
+
+const canContinue = computed(() => {
+  return (
+    broadcastsStore.newBroadcast.selectedGroups.length > 0 ||
+    (contactImportStore.import && uploadFinishedProp.value)
+  );
+});
+
+const handleGroupSelectionOpen = (value: boolean) => {
+  broadcastsStore.setGroupSelectionOpen(value);
+  broadcastsStore.setContactImportOpen(!value);
+};
+
+const handleContactImportOpen = (value: boolean) => {
+  broadcastsStore.setContactImportOpen(value);
+  broadcastsStore.setGroupSelectionOpen(!value);
+};
+
+const handleUploaded = () => {
+  emit('update:upload-finished', true);
+};
+
+const handleCancel = () => {
+  if (contactImportStore.import) {
+    contactImportStore.clearImport();
+  }
+  emit('update:upload-finished', false);
+  broadcastsStore.setNewBroadcastPage(NewBroadcastPage.SELECT_GROUPS);
+};
+
+const handleContinue = () => {
+  if (canConfirmImport.value) {
+    contactImportStore.confirmContactImport(
+      projectStore.project.uuid,
+      contactImportStore.import!.importId,
+      contactImportStore.importProcessing,
+    );
+
+    broadcastsStore.setNewBroadcastPage(NewBroadcastPage.SELECT_TEMPLATE);
+    return;
+  }
+
+  if (broadcastsStore.newBroadcast.selectedGroups.length > 0) {
+    broadcastsStore.setNewBroadcastPage(NewBroadcastPage.SELECT_TEMPLATE);
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.groups-step {
+  display: flex;
+  flex-direction: column;
+  gap: $unnnic-spacing-sm;
+  flex: 1;
+
+  &__group-selection::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: $unnnic-border-width-thinner;
+    background-color: $unnnic-color-neutral-soft;
+    margin: $unnnic-spacing-md 0;
+  }
+
+  &__selection {
+    display: flex;
+    flex-direction: column;
+    gap: $unnnic-spacing-sm;
+  }
+}
+</style>
---
+++ src/components/NewBroadcast/StepActions.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="step-actions">
+    <UnnnicButton
+      data-test="actions-cancel"
+      class="step-actions__cancel"
+      type="tertiary"
+      @click="$emit('cancel')"
+    >
+      {{ $t('new_broadcast.pages.actions.cancel') }}
+    </UnnnicButton>
+    <UnnnicButton
+      data-test="actions-continue"
+      class="step-actions__continue"
+      :disabled="disabled"
+      @click="$emit('continue')"
+    >
+      {{ $t('new_broadcast.pages.actions.continue') }}
+    </UnnnicButton>
+  </section>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  disabled?: boolean;
+}>();
+
+defineEmits<{
+  (e: 'cancel'): void;
+  (e: 'continue'): void;
+}>();
+</script>
+
+<style scoped lang="scss">
+.step-actions {
+  display: flex;
+  margin-top: auto;
+  gap: $unnnic-spacing-sm;
+
+  &__cancel,
+  &__continue {
+    flex: 1;
+  }
+}
+</style>
