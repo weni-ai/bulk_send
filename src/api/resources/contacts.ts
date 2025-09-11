5171534f54453f9401d45bce301eeca52e188643
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-09-11 10:35:42 -0300

    feat: implement contact fields API and store for managing contact data retrieval

---
+++ src/api/resources/contacts.ts
@@ -0,0 +1,33 @@
+import request from '@/api/requests';
+import { useProjectStore } from '@/stores/project';
+import type { Group } from '@/types/groups';
+
+export default {
+  async getContactFields() {
+    const { project } = useProjectStore();
+
+    const params = {
+      project_uuid: project.uuid,
+    };
+
+    const response = await request.$http.get(
+      '/api/v2/internals/contacts_fields',
+      { params },
+    );
+    return response;
+  },
+  async getContactFieldsExamplesByGroups(groups: Group[]) {
+    const { project } = useProjectStore();
+
+    const params = {
+      project_uuid: project.uuid,
+      group_ids: groups.map((group) => group.id).join(','),
+    };
+
+    const response = await request.$http.get(
+      '/api/v2/internals/groups_contact_fields',
+      { params },
+    );
+    return response;
+  },
+};
---
+++ src/stores/contact.ts
@@ -0,0 +1,49 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import {
+  type ContactField,
+  type ContactFieldWithExample,
+} from '@/types/contacts';
+import ContactAPI from '@/api/resources/contacts';
+import type { Group } from '@/types/groups';
+
+export const useContactStore = defineStore('contact', () => {
+  const contactFields = ref<ContactField[]>([]);
+  const loadingContactFields = ref(false);
+  const contactFieldsExamples = ref<ContactFieldWithExample[]>([]);
+  const loadingContactFieldsExamples = ref(false);
+
+  const fetchContactFields = async () => {
+    loadingContactFields.value = true;
+    try {
+      const response = await ContactAPI.getContactFields();
+      contactFields.value = response.data.results;
+    } catch (error) {
+      console.error(error); // TODO: handle error
+    } finally {
+      loadingContactFields.value = false;
+    }
+  };
+
+  const getContactFieldsExamplesByGroups = async (groups: Group[]) => {
+    loadingContactFieldsExamples.value = true;
+    try {
+      const response =
+        await ContactAPI.getContactFieldsExamplesByGroups(groups);
+      contactFieldsExamples.value = response.data.results;
+    } catch (error) {
+      console.error(error); // TODO: handle error
+    } finally {
+      loadingContactFieldsExamples.value = false;
+    }
+  };
+
+  return {
+    contactFields,
+    fetchContactFields,
+    loadingContactFields,
+    getContactFieldsExamplesByGroups,
+    loadingContactFieldsExamples,
+    contactFieldsExamples,
+  };
+});
