79e0a71d7799d990302ae94f87434b790cdace92
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-09-23 16:01:17 -0300

    fix: update toPercentage utility to use i18n for locale-specific formatting

--- src/utils/percentage.ts
+++ src/utils/percentage.ts
@@ -1,5 +1,7 @@
+import i18n from '@/utils/plugins/i18n';
+
 const toPercentage = (value: number): string => {
-  return `${value.toLocaleString(undefined, {
+  return `${value.toLocaleString(i18n.global.locale || undefined, {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   })}%`;
