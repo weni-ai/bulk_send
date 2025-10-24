27a56cb3e6c2a5fb3f540a4ec465d1a23237550f
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-10-24 17:15:25 -0300

    feat: implement new modals and components for broadcast confirmation and error handling

---
+++ src/components/NewBroadcast/ConfirmAndSend/parts/modals/BroadcastErrorModal.vue
@@ -0,0 +1,44 @@
+<template>
+  <UnnnicModalDialog
+    class="confirm-and-send__broadcast-errored-modal"
+    :modelValue="!!error"
+    :title="title"
+    :primaryButtonProps="{ text: primaryText }"
+    hideSecondaryButton
+    showActionsDivider
+    @primary-button-click="onBack"
+  >
+    <p class="confirm-and-send__broadcast-errored-content">
+      {{ description }}
+    </p>
+  </UnnnicModalDialog>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  error: Error | undefined;
+  title: string;
+  primaryText: string;
+  description: string;
+  onBack: () => void;
+}>();
+</script>
+
+<style scoped lang="scss">
+.confirm-and-send__broadcast-errored-modal {
+  &:deep(.unnnic-modal-dialog__container__actions) {
+    display: flex;
+    flex: 1;
+
+    button {
+      flex: 1;
+    }
+  }
+}
+
+.confirm-and-send__broadcast-errored-content {
+  @include unnnic-text-body-gt;
+  color: $unnnic-color-neutral-dark;
+  white-space: pre-wrap;
+}
+</style>
---
+++ src/components/NewBroadcast/ConfirmAndSend/parts/modals/ContactImportPendingModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <UnnnicModalDialog
+    :modelValue="modelValue"
+    :title="title"
+  >
+    <p class="confirm-and-send__contact-import-pending-content">
+      {{ content }}
+    </p>
+  </UnnnicModalDialog>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  modelValue: boolean;
+  title: string;
+  content: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+.confirm-and-send__contact-import-pending-content {
+  @include unnnic-text-body-gt;
+  color: $unnnic-color-neutral-cloudy;
+  white-space: pre-line;
+}
+</style>
